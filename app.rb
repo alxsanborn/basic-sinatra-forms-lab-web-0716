1c195b23cb3ac900e6f068d505d71ae4354c729d
jasmin47 <alxsanborn@gmail.com>
2016-08-11 17:24:07 -0400

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+get '/newteam' do
+  erb :newteam
+end
+
+post '/newteam' do
+  @name = params["name"]
+  erb :team
+end
+
+get '/team' do
+  erb :team
+end
+
 
 end
